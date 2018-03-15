431e8dc36653ce1b0e3d7ddcab5c87881d1e3bc7
Federico Cerutti <federico@ceres-c.it>
2018-03-15 13:19:53 +0100

    Updated copier and nanorc

--- copier.sh
+++ copier.sh
@@ -5,6 +5,7 @@ done
 
 sudo ln -f ./70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.rules
 sudo ln -sf $HOME/dotfiles/libnfc.conf /etc/nfc/libnfc.conf
+sudo ln -sf $HOME/dotfiles/tlp /etc/default/tlp
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
