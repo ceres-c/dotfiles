7f11e6419e6c5cd3b8b1e4cb7b976740d82bf4e3
Federico Cerutti <federico@ceres-c.it>
2020-02-17 01:11:58 +0100

    Updated config to remove conky and switched to bigger fonts to avoid scaling

--- copier.sh
+++ copier.sh
@@ -9,18 +9,17 @@ sudo ln -f ./49-micronucleus.rules /etc/udev/rules.d/49-micronucleus.rules		# Mi
 sudo ln -f ./50-usbtinyisp.rules /etc/udev/rules.d/50-usbtinyisp.rules			# To program atmel devices without sudo
 sudo ln -f ./52-usb.rules /etc/udev/rules.d/52-usb.rules						# ST CR95HF NFC board
 sudo ln -f ./70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.rules	# For USB ethernet adapters
-sudo ln -f ./77-pm3-usb-device-blacklist.rules /etc/udev/rules.d/77-pm3-usb-device-blacklist.rules # Proxmark3 ModemManager fix
+sudo ln -f ./77-pm3-usb-device-blacklist.rules /etc/udev/rules.d/77-pm3-usb-device-blacklist.rules	# Proxmark3 ModemManager fix
 sudo ln -f ./93-pn53x.rules /etc/udev/rules.d/93-pn53x.rules					# PN532 based devices without sudo
-sudo cat /etc/sysctl.d/10-local.conf											# Allows dmesg without sudo
 if sudo [ -d "/etc/nfc/" ]; then
-	sudo ln -sf $HOME/dotfiles/libnfc.conf /etc/nfc/libnfc.conf # To allow scanning of USB-UART adapters
+	sudo ln -sf $HOME/dotfiles/libnfc.conf /etc/nfc/libnfc.conf			# To allow scanning of USB-UART adapters
 else
 	echo "Libnfc does not seem to be installed. Install it and then run this script again"
 fi
 sudo ln -sf $HOME/dotfiles/tlp /etc/default/tlp
-sudo ln -sf $HOME/dotfiles/intel-undervolt.conf /etc/intel-undervolt.conf # Undervolt config for my XPS 13 9360
+sudo ln -sf $HOME/dotfiles/intel-undervolt.conf /etc/intel-undervolt.conf		# Undervolt config for my XPS 13 9360
 if [ -f "/etc/modprobe.d/blacklist-libnfc.conf" ]; then
-	sudo ln -sf $HOME/dotfiles/blacklist-libnfc.conf /etc/modprobe.d/blacklist-libnfc.conf # Modprobe offending pn533 modules
+	sudo ln -sf $HOME/dotfiles/blacklist-libnfc.conf /etc/modprobe.d/blacklist-libnfc.conf	# Modprobe offending pn533 modules
 else
 	echo "Libnfc blacklist rules not found. You might want to install libnfc first to avoid file collisions."
 fi
@@ -48,14 +47,6 @@ if [ ! -d "$HOME/.config/nano/" ]; then
 	mkdir $HOME/.config/nano/
 fi
 ln -sf $HOME/dotfiles/nanorc $HOME/.config/nano/nanorc
-if [ ! -d "$HOME/.config/conky/" ]; then
-	mkdir $HOME/.config/conky/
-fi
-ln -sf $HOME/dotfiles/conky.conf $HOME/.config/conky/conky.conf
-if [ ! -d "$HOME/.config/fusuma/" ]; then
-	mkdir ~/.config/fusuma
-fi
-ln -sf $HOME/dotfiles/fusuma_config.yml $HOME/.config/fusuma/config.yml
 if [ ! -d "$HOME/.config/pip/" ]; then
 	mkdir $HOME/.config/pip/
 fi
