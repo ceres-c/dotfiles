a20251e025d8a062ff0eff36bdf7e6ea252c28e5
Federico Cerutti <federico@ceres-c.it>
2020-05-10 12:19:10 +0200

    Updated copier script - Added resolvconf default DNS

--- copier.sh
+++ copier.sh
@@ -5,21 +5,34 @@ for i in $(ls nano); do
 done
 
 # System config
-sudo ln -rf 52-usb.rules /etc/udev/rules.d/52-usb.rules							# All USB tty and alike devices rules
-sudo ln -rf 70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.rules	# For USB ethernet adapters
-sudo ln -rsf modprobe.conf /etc/modprobe.d/modprobe.conf						# Modprobe offending pn533 modules + psmouse
-sudo ln -rf vconsole.conf /etc/vconsole.conf									# Enable bigger font and italian keymap in vConsole
-sudo ln -rf environment /etc/environment										# Global env vars
-sudo ln -f headphones_hissing.hook /etc/pacman.d/hooks/headphones_hissing.hook	# Pacman hook to fix XPS13 hissing headphones
+# All USB tty and alike devices rules
+sudo ln -rsf 52-usb.rules /etc/udev/rules.d/52-usb.rules
+# For USB ethernet adapters
+sudo ln -rsf 70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.rules
+# Modprobe offending pn533 modules + psmouse
+sudo ln -rsf modprobe.conf /etc/modprobe.d/modprobe.conf
+# Enable bigger font and italian keymap in vConsole
+sudo ln -rsf vconsole.conf /etc/vconsole.conf
+# Global env vars
+sudo ln -rsf environment /etc/environment
+# Pacman hook to fix XPS13 hissing headphones
+sudo ln -rsf headphones_hissing.hook /usr/share/libalpm/hooks/headphones_hissing.hook
 echo "[!] Don't forget to add /etc/modprobe.d/modprobe.conf to your /etc/mkinicpio.conf 'FILES' list"
 if sudo [ -d "/etc/nfc/" ]; then
-	sudo ln -rsf libnfc.conf /etc/nfc/libnfc.conf								# To allow scanning of USB-UART adapters
+    # To allow scanning of USB-UART adapters
+	sudo ln -rsf libnfc.conf /etc/nfc/libnfc.conf
 else
 	echo "Libnfc does not seem to be installed. Install it and then run this script again"
 fi
 sudo ln -rsf tlp /etc/default/tlp
-sudo ln -rsf intel-undervolt.conf /etc/intel-undervolt.conf						# Undervolt config for my XPS 13 9360
-sudo ln -f default.pa ~/.config/pulse/default.pa								# PulseAudio config to not fuck up with my docking station
+# Undervolt config for my XPS 13 9360
+sudo ln -rsf intel-undervolt.conf /etc/intel-undervolt.conf
+# Prevent dhcpcd from overwriting Google DNS. As per https://wiki.archlinux.org/index.php/Dhcpcd#/etc/resolv.conf
+sudo ln -rsf resolv.conf.head /etc/resolv.conf.head
+# Terminator KDE ServiceMenu entry
+sudo ln -rsf openTerminatorHere.desktop /usr/share/kservices5/openTerminatorHere.desktop
+# Increase number of inotify watchers. Done with tee as echo was dropped privileges befor writing
+echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf > /dev/null
 
 # root user config
 sudo ln -rsf .tmux.conf /root/.tmux.conf
@@ -33,9 +46,13 @@ fi
 sudo ln -rsf nanorc /root/.config/nano/nanorc
 
 # Current user config
-mkdir "$HOME/.config/terminator/"
+# PulseAudio config to not fuck up with my docking station
+ln -rsf default.pa ~/.config/pulse/default.pa
+# Miscellaneous user software
+if [ ! -d "$HOME/.config/terminator/" ]; then
+	mkdir $HOME/.config/terminator/
+fi
 ln -rsf terminator.conf $HOME/.config/terminator/config
-ln -rf openTerminatorHere.desktop $HOME/.local/share/kservices5/ServiceMenus/openTerminatorHere.desktop
 ln -rsf alacritty.yml $HOME/.alacritty.yml
 ln -rsf .tmux.conf $HOME/.tmux.conf
 ln -rsf .tmux $HOME/.tmux
@@ -49,15 +66,13 @@ ln -rsf nanorc $HOME/.config/nano/nanorc
 if [ ! -d "$HOME/.config/pip/" ]; then
 	mkdir $HOME/.config/pip/
 fi
-echo "[!] You don't want to use pip with the --user switch anymore. Please do things the right way this time and fix this mess.\nAlso, remove the PYTHONPATH export in .zshrc"
+echo "[!] You don't want to use pip with the --user switch anymore. Please do things the right way this time and fix this mess."
+echo "	Also, remove the PYTHONPATH export in .zshrc"
 ln -rsf pip.conf $HOME/.config/pip/pip.conf
-
-# Increase number of inotify watchers
-echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf
 
 # MPV config for VAAPI
 if [ ! -d "$HOME/.config/mpv/" ]; then
     mkdir $HOME/.config/mpv/
 fi
-echo "[#] Install `intel-media-driver` if not already done"
+echo "[#] Install intel-media-driver if not already done"
 ln -rsf mpv.conf $HOME/.config/mpv/mpv.conf
