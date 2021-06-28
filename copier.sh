544da6a18d3106907f590e169ac70acb3f2f1ba9
Federico Cerutti <federico@ceres-c.it>
2021-06-28 18:43:50 +0200

    Fixed coper bugs, removed useless files, added further dot

--- copier.sh
+++ copier.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Install required packages
+yay -S zsh-theme-powerlevel10k prezto-git nerd-fonts-source-code-pro ssh-agent-service gnome-keyring intel-media-driver
+
 # Nano syntax highlight
 for i in $(ls nano); do
 	sudo ln -rsf nano/$i /usr/share/nano/$i
@@ -7,8 +11,6 @@ done
 # System config
 # All USB tty and alike devices rules
 sudo ln -rsf 52-usb.rules /etc/udev/rules.d/52-usb.rules
-# For USB ethernet adapters
-sudo ln -rsf 70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.rules
 # Logitech c930 Webcam config applied on boot (brightness + power live frequency)
 sudo ln -rsf 99-logitech-c930-video.rules /etc/udev/rules.d/99-logitech-c930-video.rules
 # Modprobe offending pn533 modules + psmouse
@@ -19,12 +21,11 @@ sudo ln -rsf vconsole.conf /etc/vconsole.conf
 # Global env vars
 sudo ln -rsf environment /etc/environment
 # LibNFC settings
-if sudo [ -d "/etc/nfc/" ]; then
-    # To allow scanning of USB-UART adapters
-	sudo ln -rsf libnfc.conf /etc/nfc/libnfc.conf
-else
-	echo "Libnfc does not seem to be installed. Install it and then run this script again"
+if sudo [ ! -d "/etc/nfc/" ]; then
+	sudo mkdir /etc/nfc/
 fi
+# To allow scanning of USB-UART adapters
+sudo ln -rsf libnfc.conf /etc/nfc/libnfc.conf
 # TLP config
 sudo ln -rsf tlp /etc/default/tlp
 # Undervolt config for my XPS 13 9360
@@ -37,6 +38,10 @@ sudo ln -rsf openTerminatorHere.desktop /usr/share/kservices5/openTerminatorHere
 sudo ln -rsf 40-max-user-watches.conf /etc/sysctl.d/40-max-user-watches.conf
 # Restore old (possibly dangerous) dmesg nonroot behaviour
 sudo ln -rsf 51-dmesg-restrict.conf /etc/sysctl.d/51-dmesg-restrict.conf
+# Force polkit to ask for root password
+sudo cp 49-rootpw_global.rules /etc/polkit-1/rules.d/49-rootpw_global.rules # Can't symlink this file as polkit wouldn't import the rule
+# Use traditional interface naming scheme
+sudo ln -rsf /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 
 # root user config
 sudo ln -rsf .tmux.conf /root/.tmux.conf
@@ -46,19 +51,18 @@ sudo ln -rsf .zpreztorc /root/.zpreztorc
 sudo ln -rsf .gitconfig /root/.gitconfig
 sudo ln -rsf .pam_environment /root/.pam_environment
 if sudo [ ! -d "/root/.config/nano/" ]; then
-	sudo mkdir /root/.config/nano/
+	sudo mkdir -p /root/.config/nano/
 fi
 sudo ln -rsf nanorc /root/.config/nano/nanorc
 
 # Current user config
-# PulseAudio config to not fuck up with my docking station
-ln -rsf default.pa ~/.config/pulse/default.pa
 # Xorg config to unlock gnome-keyring used by vscode on login
 ln -rsf .xprofile $HOME/.xprofile
 # Miscellaneous user software
 if [ ! -d "$HOME/.config/terminator/" ]; then
 	mkdir $HOME/.config/terminator/
 fi
+ln -rsf kwalletrc $HOME/.config/kwalletrc # Disable kwallet popups
 ln -rsf terminator.conf $HOME/.config/terminator/config
 ln -rsf alacritty.yml $HOME/.alacritty.yml
 ln -rsf .tmux.conf $HOME/.tmux.conf
@@ -77,13 +81,15 @@ echo "[!] You don't want to use pip with the --user switch anymore. Please do th
 echo "	Also, remove the PYTHONPATH export in .zshrc"
 ln -rsf pip.conf $HOME/.config/pip/pip.conf
 # Pacman hook to fix XPS13 hissing headphones
+if [ ! -d "$HOME/.config/systemd/user/" ]; then
+    mkdir -p $HOME/.config/systemd/user/
+fi
 ln -rsf headphones_hissing.service $HOME/.config/systemd/user/headphones_hissing.service
 systemctl --user enable headphones_hissing.service
 # start ssh-agent on login
-ln -rsf ssh-agent.service $HOME/.config/systemd/user/ssh-agent.service
 systemctl --user enable ssh-agent.service
 # Enable ssg-agent as the default pam ssh auth service
-ln -rsf .pam_environment /root/.pam_environment
+sudo ln -rsf .pam_environment /root/.pam_environment
 
 if [ ! -d "$HOME/.config/keepassxc/" ]; then
 	mkdir $HOME/.config/keepassxc/
